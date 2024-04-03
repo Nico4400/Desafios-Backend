bfc17044c564ab3a32095813b48e375c7a35e311
Nico Fernandez <nico.fernandezcastillo@gmail.com>
2024-04-02 22:34:52 -0300

    Práctica de integración sobre tu ecommerce

---
+++ Desafio-12/src/config/config.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+export const getVariables = (options) => {
+    const environment = options.opts().mode;
+    // console.log(`Environment: ${environment}`);
+    dotenv.config({
+        path: environment === 'production' ? './.env.production' : './.env.development'
+    });
+
+    // console.log(`PORT: ${process.env.PORT}`);
+    // console.log(`MONGO_URL: ${process.env.MONGO_URL}`);
+    // console.log(`adminName: ${process.env.ADMIN_NAME}`);
+    // console.log(`adminPassword: ${process.env.ADMIN_PASSWORD}`);
+    // console.log(`persistence: ${process.env.PERSISTENCE}`);
+    // console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+    return {
+        PORT: process.env.PORT,
+        MONGO_URL: process.env.MONGO_URL,
+        adminName: process.env.ADMIN_NAME,
+        adminPassword: process.env.ADMIN_PASSWORD,
+        persistence: process.env.PERSISTENCE,
+        googlePass: process.env.GOOGLE_PASSWORD,
+        NODE_ENV: process.env.NODE_ENV
+    }
+}
